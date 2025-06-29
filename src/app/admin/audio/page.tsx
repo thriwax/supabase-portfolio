b403e16daa64217f9b76a6cb4fb5eb561771d55f
Fedor Tatarintsev <91975849+thriwax@users.noreply.github.com>
2025-06-29 08:57:24 +0300

    fix: правильный экспорт страницы audio

--- src/app/admin/audio/page.tsx
+++ src/app/admin/audio/page.tsx
@@ -5,7 +5,7 @@ import { supabase } from '../../../../lib/supabaseClient'
 import { uploadImage } from '../../../../lib/uploadImage'
 import TrackList from '@/app/components/TrackList/TrackList'
 
-export default function AudioUploader() {
+export default function Page() {
     const [title, setTitle] = useState('')
     const [audioFile, setAudioFile] = useState<File | null>(null)
     const [coverFile, setCoverFile] = useState<File | null>(null)
@@ -55,7 +55,6 @@ export default function AudioUploader() {
             />
             <input type="file" accept="audio/*" onChange={(e) => setAudioFile(e.target.files?.[0] || null)} />
             <input type="file" accept="image/*" onChange={(e) => setCoverFile(e.target.files?.[0] || null)} />
-            {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
             <button onClick={handleUpload} className="bg-black text-white px-4 py-2 rounded">Загрузить</button>
             <p className="text-sm text-gray-600">{message}</p>
             <TrackList />
